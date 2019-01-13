a06b943c640c0ee74d0c0cb8c7639172250c3021
Irene Saez <bluemilk44@gmail.com>
2019-01-13 22:57:47 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase, say="Squawk!")
+def parrot(phrase, say="Squawk!", phrase="Squawk!")
   puts phrase
 end
